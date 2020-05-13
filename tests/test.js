38052736cae30e2dc62a07efbf2b206e90527291
t0ri <victoriarosemurray@gmail.com>
2020-05-12 18:40:21 -0700

    complete testing

--- tests/test.js
+++ tests/test.js
@@ -58,6 +58,7 @@ describe('Prefix Tree Node', () => {
       expect(node).toHaveProperty('terminal', false)
     })
   })
+
   describe('methods return correct values', () => {
     strings = ['testing', 'code', 'is', 'tedious']
     tree = new PrefixTree(strings)
@@ -70,6 +71,82 @@ describe('Prefix Tree Node', () => {
     test('- childrenCount', () => {
       expect(terminalNode.childrenCount()).toBe(0)
       expect(nonTerminalNode.childrenCount()).toBe(2)
+    })
+    test('- getChild throws error', () => {
+      expect( () => {
+        nonTerminalNode.getChild('a')
+      } ).toThrowError(new Error('No child exists for character a'))
+    })
+    test('- addChild throws error', () => {
+      expect( () => {
+        nonTerminalNode.addChild('s')
+      } ).toThrowError(new Error('Child already exists for character s'))
+    })
+  })
+})
+
+describe('Autocomplete', () => {
+  describe('initializes', () => {
+    test('without data', () => {
+      autocomplete = new Autocompletion()
+      expect(autocomplete).toBeInstanceOf(Autocompletion)
+      expect(autocomplete).toHaveProperty('entries', 0)
+      expect(autocomplete).toHaveProperty('entriesCharCount', 0)
+      expect(autocomplete).toHaveProperty('autocompleteCount', undefined)
+      expect(autocomplete).toHaveProperty('ignoreCasing', true)
+    })
+    test('with data', () => {
+      strings = ['testing', 'code', 'is', 'tedious']
+      autocomplete = new Autocompletion(strings, 1, false)
+      expect(autocomplete).toBeInstanceOf(Autocompletion)
+      expect(autocomplete).toHaveProperty('entries', 4)
+      expect(autocomplete).toHaveProperty('entriesCharCount', 18)
+      expect(autocomplete).toHaveProperty('autocompleteCount', 1)
+      expect(autocomplete).toHaveProperty('ignoreCasing', false)
+    })
+  })
+
+  describe('methods return correct values', () => {
+    describe('- autocomplete', () => {
+      test('ignores casing', () => {
+        strings = ['Testing', 'code', 'is', 'tedious']
+        autocomplete = new Autocompletion(strings)
+        expect(autocomplete.autocomplete('t')).toStrictEqual(['testing', 'tedious'])
+        strings = ['Testing', 'code', 'is', 'tedious']
+        autocomplete = new Autocompletion(strings, 2, false)
+        expect(autocomplete.autocomplete('T')).toStrictEqual(['Testing'])
+      })
+      test('utilizes autocompleteCount', () => {
+        strings = ['testing', 'code', 'is', 'tedious']
+        autocomplete = new Autocompletion(strings, 1)
+        expect(autocomplete.autocomplete('t')).toStrictEqual(['testing'])
+      })
+      test('adds default prefix', () => {
+        strings = ['testing', 'code', 'is', 'tedious']
+        autocomplete = new Autocompletion(strings)
+        expect(autocomplete.autocomplete()).toStrictEqual(['testing', 'tedious', 'code', 'is'])
+      })
+    })
+    
+    describe('- addEntry', () => {
+      test('checks for entry', () => {
+        autocomplete = new Autocompletion()
+        autocomplete.addEntry()
+        expect(autocomplete.entries).toBe(0)
+      })
+    })
+
+    describe('- addEntries', () => {
+      test('checks for entries', () => {
+        autocomplete = new Autocompletion()
+        autocomplete.addEntries()
+        expect(autocomplete.entries).toBe(0)
+      })
+      test('checks entries are strings', () => {
+        autocomplete = new Autocompletion()
+        autocomplete.addEntries(['hi', 2])
+        expect(autocomplete.entries).toBe(1)
+      })
     })
   })
 })
